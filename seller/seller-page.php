09a0dbd071ff8651b4c290b3be68cf5ccb1db769
orayanics <114231058+orayanics@users.noreply.github.com>
2022-10-22 12:54:31 +0800

    Revert "seller"
    This reverts commit ffb6403f301b5a987e33756e951406fad4de89b2.

--- seller/seller-page.php
+++ seller/seller-page.php
@@ -1,6 +1,6 @@
 <?php
 
-
+require_once '../templates/navbar_seller.php';
 require_once '../config.php';
 session_start();
 if (isset($_SESSION['isLogin'])) {
@@ -34,35 +34,37 @@ if (isset($_SESSION['isLogin'])) {
 </head>
 
 <body>
-<?php require_once '../templates/navbar_seller.php'; ?>
+
+     <!-- Navbar -->
+    <?php require_once '../templates/navbar_seller.php' ?>
+
     <!-- MAIN CONTENT -->
-    <main class="full-container" style="margin-top:5%;">
-        <img src="../img/s1.jpg" alt="" id="sellerIcon" class="profile-icon">
+    <main class="full-container"style="margin-top:5%;">
+        <img src="../img/s1.jpg" alt="" id="buyerIcon" class="profile-icon">
         <div class="solid-bg">
-            <h1 id="sellerName" class="center"><?php echo $_SESSION["sFirstName"] ?> <?php echo $_SESSION["sLastName"] ?></h1>
-            <p id="productNum" class="center">Products: </p>
+            <h1 id="buyerName" class="center">
+                <!-- NAME -->
+                <?php echo $_SESSION["sFirstName"]?> <?php echo $_SESSION["sLastName"] ?>
+            </h1>
             <div class="flex row">
-                <p id="sellerPhone">Number: <?php echo $_SESSION["sPhoneNumber"] ?> </p>
-                <p id="sellerAddr">Address: <?php echo $_SESSION["sAddress"] ?> </p>
+                <p id="buyerPhone">Number:
+                <?php echo $_SESSION["sPhoneNumber"]?> 
+                </p>
+                <p id="buyerAddr">Address:  
+                    <?php echo $_SESSION["sAddress"]?> 
+            </p>
+
             </div>
-            <button class="btn-circle btn-center">
-                <a href="acc-profile-seller.php" style="color: white;text-decoration: none;">
-                    Edit Profile </a>
-            </button>
+            
+            <button class="btn-circle btn-center" > <a href="acc-profile-buyer.php" style="color: white" >
+                Edit Profile </a></button>
         </div>
+        
+        
     </main>
 
-    <section class="half-container" style="margin-top: 10%;">
-        <div class="flex row">
-            <div class="product"></div>
-            <div class="product"></div>
-            <div class="product"></div>
-        </div>
-
-    </section>
-
     <footer>
-
+        
     </footer>
 
 </body>
