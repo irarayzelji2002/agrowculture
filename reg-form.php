736ce9ed7d6e8eb5a3c0b846b061e07524f6254d
orayanics <114231058+orayanics@users.noreply.github.com>
2022-10-22 16:16:07 +0800

    responsive na reg-form
    IM SO HUNGRY MAAAN for good grades

--- reg-form.php
+++ reg-form.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!-- MAIN CSS Sheet-->
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/reg-form-style.css">
@@ -14,103 +15,108 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    
-    
+
+
     <title>aGROWculture</title>
 
 </head>
+
 <body>
-    
-    <!-- Navbar -->
-    <nav class="sticky">
-        <div class="flex-nav">
-        <div>aGROWculture</div>
-        <div>
-            <a href="" class="nav-link">SELL</a>
-            <a href="" class="nav-link">LOGIN</a>
-            <a href="" class="nav-link">SIGN UP</a>
-            <a href="" class="nav-link">ICON LOGO</a>
-        </div>
+    <!--NAVBAR-->
+    <?php require_once 'templates/navbar.php' ?>
+    <div class="bg-image"></div>
+    <main class="absolute-container">
+        <div class="form-container solid-bg">
+            <form class="form-flex column" id="regForm" action="db/registerDATA.php" method="post">
+                <div>
+                    <div>
+                        <h1>Join our community.</h1>
+                        <!--Role-->
+                        <h3 class="sub-link">I am a...</h3>
+                    </div>
 
-        </div>
-    </nav>
+                    <div class="form-flex">
+                        <label>
+                            <p class="sub-link">Buyer</p>
+                            <input type="radio" id="Buyer" name="user_type" value="Buyer" checked="checked" style="margin-right: 2rem;" />
+                        </label>
 
-    <!--REGFORM-->
-    <section>
-        <!--Background Image-->
-        <section class="bg-image"></section>
-        <!--Form-->
-        <div class="container">
-            <form id="regForm" class="regform" action="db/registerDATA.php" method="post">
-                <h1>Join our community.</h1>
-                <!--Role-->
-                <h3 class="formGreenText">I am a...</h3>
-                <div class="left-input">
-                    <label style>
-                    <p class="formGreenText">Buyer</p>
-                    <input type="radio" id="Buyer" name="user_type" value="Buyer" checked="checked"/>
-                    </label>
-                </div>
-                <div class="right-input">
-                    <label>
-                    <p class="formGreenText">Seller</p>
-                    <input type="radio" id="Seller" name="user_type" value="Seller"/>
-                    </label>
-                </div>
-                <!--Account Details-->
-                <fieldset id="acctDetails">
-                    <legend><h2 style="margin-top: 2rem;">Account Details</h2></legend>
-                    <div style="position: relative; margin-right: 8px;">
-                        <div class="left-input">
-                            <p class="formGreenText">First Name</p>
-                            <div class="wrapper"><input type="text" id="first_name" name="first_name" required/></div>
-                        </div>
+                        <label>
+                            <p class="sub-link">Seller</p>
+                            <input type="radio" id="Seller" name="user_type" value="Seller" />
+                        </label>
                     </div>
-                    <div style="position: relative; margin-left: 8px;">
-                        <div class="right-input">
-                            <p class="formGreenText">Last Name</p>
-                            <div class="wrapper"><input type="text" id="last_name" name="last_name" required/></div>
-                        </div>
-                    </div>
-                    <p class="formGreenText">Email Address</p>
-                    <div class="wrapper"><input type="text" id="email" name="email" required/></div>
-                    <p class="formGreenText">Password</p>
-                    <div class="wrapper"><input type="text" id="password" name="password" required/></div>
-                </fieldset></br>
-                <!--Business Details-->
-                <fieldset id="businessDetails">
-                    <legend><h2>Business Details</h2></legend>
-                    <p class="formGreenText">Company/Business Name</p>
-                    <div class="wrapper"><input type="text" id="business_name" name="business_name" required/></div>
-                    <p class="formGreenText">Phone Number</p>
-                    <div class="wrapper"><input type="text" id="phone_number" name="phone_number" required/></div>
-                    <p class="formGreenText">Address</p>
-                    <div class="wrapper"><input type="text" id="address" name="address" required/></div>
-                </fieldset></br>
-                <!--Upload Documents-->
-                <fieldset id="uploadDocuments">
-                    <legend><h2 class="formGreenText">Upload Documents</h2></legend>
-                    <div class="left-input">
-                        <div class="upload-btn-wrapper">
-                            <button class="btn">SELECT FILES</button>
-                            <input type="file" name="myfile"></input>
-                        </div>
-                    </div>
-                </fieldset>
-                <!--Submit Button-->
-                <div class="contentButton wrapper">
-                    <button type="submit" class="btn-box">SUBMIT</button>
+
                 </div>
+
+
+
+                <section class="fix-width">
+                    <div>
+                        <!--Account Details-->
+                        <fieldset id="acctDetails">
+                            <legend>
+                                <h2 style="margin-top: 2rem;">Account Details</h2>
+                            </legend>
+
+                            <div class="form-flex">
+                                <div>
+                                    <p class="sub-link">First Name</p>
+                                    <div><input type="text" id="first_name" name="first_name" required /></div>
+                                </div>
+
+                                <div>
+                                    <p class="sub-link">Last Name</p>
+                                    <div><input type="text" id="last_name" name="last_name" required /></div>
+                                </div>
+                            </div>
+
+                            <p class="sub-link">Email Address</p>
+                            <div><input type="text" id="email" name="email" required /></div>
+                            <p class="sub-link">Password</p>
+                            <div><input type="text" id="password" name="password" required /></div>
+                        </fieldset></br>
+                    </div>
+
+                    <div>
+                        <!--Business Details-->
+                        <fieldset id="businessDetails">
+                            <legend>
+                                <h2>Business Details</h2>
+                            </legend>
+                            <p class="sub-link">Company/Business Name</p>
+                            <div><input type="text" id="business_name" name="business_name" required /></div>
+                            <p class="sub-link">Phone Number</p>
+                            <div><input type="text" id="phone_number" name="phone_number" required /></div>
+                            <p class="sub-link">Address</p>
+                            <div><input type="text" id="address" name="address" required /></div>
+                        </fieldset></br>
+                    </div>
+
+
+                    <div>
+                        <!--Upload Documents-->
+                        <fieldset id="uploadDocuments">
+                            <legend>
+                                <h2 class="sub-link">Upload Documents</h2>
+                            </legend>
+                            <input type="file" name="myfile" class="upload-btn-wrapper"></input>
+                        </fieldset>
+                    </div>
+
+                    <!--Submit Button-->
+                    <div class="contentButton wrapper">
+                        <button type="submit" class="btn-box">SUBMIT</button>
+                    </div>
             </form>
-            <!--Picture on Right-->
-            <div class="right-container">
-                <img src="img/farm.jpg" style="width: 100%; height: 100%; object-fit: cover;" alt="picture">
-            </div>
         </div>
-    </section>
-    
+
+        </section>
+    </main>
+
     <!--FOOTER-->
-    <footer>  
+    <footer>
     </footer>
 </body>
+
 </html>
