0e369eeffd8a05ff6970de83d3dc55557ca5f189
caryylleee <106076745+caryylleee@users.noreply.github.com>
2022-11-14 19:03:46 +0800

    final reg-form.php

--- reg-form.php
+++ reg-form.php
@@ -8,37 +8,36 @@
 
     <!-- MAIN CSS Sheet-->
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/reg-form-style.css">
-    
+    <link rel="stylesheet" href="css/reg-form.css">
 
     <!-- Google Font: Poppins-->
     <!-- Weights: 400 REGULAR, 600 SEMIBOLD, 700 BOLD -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    
-    <!-- FAVICON -->
-    <link rel="icon" href="img/aGROWculture-Favicon.png" type="image/gif" sizes="16x16">
-    
+
+      <!-- FAVICON -->
+  <link rel="icon" href="img/aGROWculture-Favicon.png" type="image/gif" sizes="16x16">
     <title>aGROWculture</title>
 
 </head>
 
-<body class="bg-image">
+<body>
     <!--NAVBAR-->
-    <?php require_once 'templates/navbar.php' ?>
-      <main class="absolute-container" style="padding-top: 20%;">
-        <div class="form-container solid-bg">
+    <?php require_once 'templates/navbar_index.php' ?>
+    <div class="bg-image"></div>
+    <main class="absolute-container test1" style="margin-top: 10%;">
+        <div class="form-container test1 solid-bg">
             <form class="form-flex column" id="regForm" action="db/registerDATA.php" method="post" enctype="multipart/form-data">
                 <div>
                     <div>
-                        <h1 style="font-weight:900; font-size:30px;">Join our community.</h1>
+                        <h1 style="color: black; font-weight:900; font-size:24px;">Join our community.</h1>
                         <!--Role-->
-                        <h3 class="sub-link" style="font-weight:900; font-size:19px; margin-top:1rem;">I am a...</h3>
+                        <h3 class="sub-link">I am a...</h3>
                     </div>
 
                     <div class="form-flex">
-                        <label style="display: inline-block">
+                        <label>
                             <span class="sub-link">Buyer</span>
                             <input type="radio" id="Buyer" name="user_type" value="Buyer" checked="checked" style="margin-right: 2rem;" />
                         </label>
@@ -58,7 +57,7 @@
                         <!--Account Details-->
                         <fieldset id="acctDetails">
                             <legend>
-                                <h2 style="margin-top:1rem; font-weight:900; font-size:24px;">Account Details</h2>
+                                <h2 style="margin-top: 2rem; color: black; font-weight:900; font-size:24px;">Account Details</h2>
                             </legend>
 
                             <div class="form-flex">
@@ -76,7 +75,7 @@
                             <p class="sub-link">Email Address</p>
                             <div><input type="text" id="email" name="email" required /></div>
                             <p class="sub-link">Password</p>
-                            <div><input type="text" id="password" name="password" required /></div>
+                            <div><input type="password" id="password" name="password" required /></div>
                         </fieldset></br>
                     </div>
 
@@ -95,7 +94,7 @@
                         <br>
                         <fieldset id="businessDetails">
                             <legend>
-                                <h2 style="font-weight:900; font-size:24px;">Business Details</h2>
+                                <h2 style="margin-top: 2rem; color: black; font-weight:900; font-size:24px;">Business Details</h2>
                             </legend>
                             <p class="sub-link">Company/Business Name</p>
                             <div><input type="text" id="business_name" name="business_name" required /></div>
@@ -124,8 +123,8 @@
         </div>
 
         </section>
-      </main>
-    </div>
+    </main>
+
     <!--FOOTER-->
     <footer>
     </footer>
